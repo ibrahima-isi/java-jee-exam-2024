308dc384d8f3ed0db1a1dbc88ccffb1eb32adc7d
Ibrahima Diallo <ibrahimadiallo@groupeisi.com>
2024-06-24 14:17:07 +0000

    Location properties

--- src/main/java/gn/dev/examainjee2024/entity/Location.java
+++ src/main/java/gn/dev/examainjee2024/entity/Location.java
@@ -1,4 +1,34 @@
 package gn.dev.examainjee2024.entity;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Table
 public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long locationID;
+
+    /**
+     * Relation to the tenant of the location
+     */
+    @ManyToOne
+    @JoinColumn(name = "tenant_id")
+    private User user;
+
+    /**
+     * Relation to the appartment of the location
+     */
+    @ManyToOne
+    @JoinColumn(name = "appartment_id")
+    private Appartment appartment;
+
+    /**
+     * The timestamp is on LocalTime so that we can have the hour if needed
+     */
+    @Column
+    @Temporal(TemporalType.DATE)
+    private LocalDate locationDate;
 }
