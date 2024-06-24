84a1c82d2faeed5fc0c5611d9e5002b96b02bc1b
Ibrahima Diallo <ibrahimadiallo@groupeisi.com>
2024-06-24 14:59:42 +0000

    Payment entity properties

--- src/main/java/gn/dev/examainjee2024/entity/Payment.java
+++ src/main/java/gn/dev/examainjee2024/entity/Payment.java
@@ -1,6 +1,8 @@
 package gn.dev.examainjee2024.entity;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.YearMonth;
 
 @Entity
 @Table
@@ -8,6 +10,16 @@ public class Payment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long paymentID;
+    /**
+     * the month that have been paid
+     */
+    @Column
+    private YearMonth month;
+    /**
+     * The day that the payment have been made.
+     */
+    @Column
+    private LocalDate paymentDate;
 
     /**
      * the status of the entity, if defined to true then the entity will be listed, if not, he will be in archived
@@ -21,5 +33,12 @@ public class Payment {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    /**
+     * Relation to the location that have been paid by this payment
+     */
+    @ManyToOne
+    @JoinColumn(name = "location_id")
+    private Location location;
 
 }
